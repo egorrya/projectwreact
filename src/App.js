1a13cf6c6d81eb97ab8a0b27db3b5b77071d0f1b
soldsite <fayncore@gmail.com>
2020-05-16 17:29:48 +0700

    static stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import GoalList from "./components/GoalList";
+import "./App.css";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./GoalList.css";
+
+const GoalList = () => {
+  return (
+    <ul className="goals-list">
+      <li>Finish the course</li>
+      <li>Learn the course</li>
+    </ul>
+  );
+};
+
+export default GoalList;
